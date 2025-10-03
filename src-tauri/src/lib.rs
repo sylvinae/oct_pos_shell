661bae912f4780801d9f4d9faa5b6a64f7c73867
je.subelario <subelelario@gmail.com>
2025-10-04 05:30:52 +0800

    Add automatic update checker on app startup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -45,6 +45,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             get_app_version,
             get_platform,
@@ -52,6 +53,40 @@ pub fn run() {
             print_receipt,
             show_alert
         ])
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                let handle = app.handle().clone();
+                tauri::async_runtime::spawn(async move {
+                    let _ = check_for_updates(handle).await;
+                });
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[cfg(desktop)]
+async fn check_for_updates(app: tauri::AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    use tauri_plugin_updater::UpdaterExt;
+    
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+        
+        update.download_and_install(
+            |chunk_length, content_length| {
+                downloaded += chunk_length;
+                println!("Downloaded {}/{:?}", downloaded, content_length);
+            },
+            || {
+                println!("Download finished, installing...");
+            },
+        ).await?;
+        
+        println!("Update installed successfully!");
+        app.restart();
+    }
+    
+    Ok(())
 }
