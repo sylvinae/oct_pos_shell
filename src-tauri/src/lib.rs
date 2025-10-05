49379cf920873f0a7434ca65922ea0ca7e358466
je.subelario <subelelario@gmail.com>
2025-10-06 07:42:32 +0800

    working tauri script

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -111,6 +111,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             get_app_version,
